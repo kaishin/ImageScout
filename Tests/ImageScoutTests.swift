95d3c0b8e30f5b3484c86c7bdf6e15ae52c12170
Reda Lemeden <git@kaishin.haz.email>
2016-06-24 20:25:56 +0200

    Disable PNG compression

--- Tests/ImageScoutTests.swift
+++ Tests/ImageScoutTests.swift
@@ -18,7 +18,7 @@ class ImageScoutTests: XCTestCase {
     let expectation = self.expectation(withDescription: "Scout JPEG images")
     let imagePath = Bundle(for: ImageScoutTests.self).urlForResource("scout", withExtension: "jpg")!
 
-    scout.scoutImageWithURL(imagePath) { (error, size, type) -> () in
+    scout.scoutImage(atURL: imagePath) { (error, size, type) -> () in
       expectation.fulfill()
       XCTAssertEqual(size, CGSize(width: 500, height: 375), "Image size should be 500 by 375")
       XCTAssertEqual(type, ScoutedImageType.jpeg, "Image type should be JPEG")
@@ -33,7 +33,7 @@ class ImageScoutTests: XCTestCase {
     let expectation = self.expectation(withDescription: "Scout PNG images")
     let imagePath = Bundle(for: ImageScoutTests.self).urlForResource("scout", withExtension: "png")!
 
-    scout.scoutImageWithURL(imagePath) { (error, size, type) -> () in
+    scout.scoutImage(atURL: imagePath) { (error, size, type) -> () in
       expectation.fulfill()
       XCTAssertEqual(size, CGSize(width: 500, height: 375), "Image size should be 500 by 375")
       XCTAssertEqual(type, ScoutedImageType.png, "Image type should be PNG")
@@ -48,7 +48,7 @@ class ImageScoutTests: XCTestCase {
     let expectation = self.expectation(withDescription: "Scout GIF images")
     let imagePath = Bundle(for: ImageScoutTests.self).urlForResource("scout", withExtension: "gif")!
 
-    scout.scoutImageWithURL(imagePath) { (error, size, type) -> () in
+    scout.scoutImage(atURL: imagePath) { (error, size, type) -> () in
       expectation.fulfill()
       XCTAssertEqual(size, CGSize(width: 500, height: 375), "Image size should be 500 by 375")
       XCTAssertEqual(type, ScoutedImageType.gif, "Image type should be GIF")
@@ -63,7 +63,7 @@ class ImageScoutTests: XCTestCase {
     let expectation = self.expectation(withDescription: "Ignore unsupported formats")
     let imagePath = Bundle(for: ImageScoutTests.self).urlForResource("scout", withExtension: "bmp")!
 
-    scout.scoutImageWithURL(imagePath) { (error, size, type) -> () in
+    scout.scoutImage(atURL: imagePath) { (error, size, type) -> () in
       expectation.fulfill()
       XCTAssertEqual(size, CGSize.zero, "Image size should be 0 by 0")
       XCTAssertEqual(type, ScoutedImageType.unsupported ,"Image type should be Unsupported")
